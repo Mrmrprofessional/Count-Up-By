55d7799fd0b8eae8325864113ca4f5fd591268a4
mike rapp & kelli margaret <student@epicodus.com>
2015-09-10 11:01:47 -0700

    Create function to count. Add HTML.

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,21 @@
-var words = function(words){
-    var split = words.split(" ");
-    var final = "";
-    for(var i = 0; i < split.length; i++){
-        var latin = pigLatin(split[i]);
-        final += latin + " ";
-        var result = final.substr(0, final.length-1);
+var countUpTo = function(countUp, countTo){
+    var numbers = [];
+    for(var i = countUp; i <= countTo; i += countUp){
+        numbers.push(i);
     }
-    return result;
+    return numbers;
 }
 
 
-var pigLatin = function(string){
-    var counter = 0;
-    var lowerString = string.toLowerCase();
-    if(lowerString[0].match(/[q]/g)){
-        var add = lowerString + "qu";
-        var slice = add.slice(2);
-        counter++;
-        var result = pigLatin(slice);
-    }else if(lowerString[0].match(/[y]/g) && counter > 0){
-        var add = lowerString + "y";
-        var slice = add.slice(1);
-        counter++;
-        var result = pigLatin(slice);
-    }else if(lowerString[0].match(/[aeiouy]/g)){
-        var result = lowerString + 'ay';
-    }else{
-        var add = lowerString + lowerString.charAt(0);
-        var slice = add.slice(1);
-        counter++;
-        var result = pigLatin(slice);
-    }
-    return result;
-}
-
-
-
-
-$(document).ready(function() {
-    $("form#text").submit(function(event){
-        var string = $("input#string").val();
-        var result = words(string);
-        $(".string").text(result);
+$(document).ready(function(){
+    $("form#numbers").submit(function(event) {
+        var countUp = parseInt($("input#countUp").val());
+        console.log(countUp);
+        var countTo = parseInt($("input#countTo").val());
+        var result = countUpTo(countUp, countTo);
+        console.log(result);
+        $(".answer").text(result.join(", "));
         $("#result").show();
         event.preventDefault();
     });
 });
-
-// $(document).ready(function() {
-//   $("form#text").submit(function(event) {
-//     var year = parseInt($("input#year").val());
-//     var result = leapYear(year);
-//
-//     $(".year").text(year);
-//     if(!result) {
-//       $(".not").text("not");
-//     } else {
-//       $(".not").empty();
-//     }
-//
-//     $("#result").show();
-//     event.preventDefault();
-//   });
-// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,13 @@
 describe('countUp', function() {
     it("counts to a number by one", function(){
-        expect(coutUp(10,1)).to.equal(1,2,3,5,6,7,8,9,10);
+        expect(countUpTo(1,10)).to.eql([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    });
+
+    it("starts counting from countUp to countTo by countUp", function(){
+        expect(countUpTo(5,25)).to.eql([5,10,15,20,25]);
+    });
+
+    it("starts counting from countUp to countTo by countUp when the countUp is not a factor of countTo", function(){
+        expect(countUpTo(5,24)).to.eql([5,10,15,20]);
     });
 });
